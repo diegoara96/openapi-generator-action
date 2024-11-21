a917e06ae595044797ebbd6366a482e882094b6d
daraujo <daraujo@gradiant.org>
2024-11-21 20:34:36 +0100

    fix broken path

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
   apt update
   apt install -y curl jq maven
+  mkdir -p ~/bin/openapitools
   curl https://raw.githubusercontent.com/OpenAPITools/openapi-generator/master/bin/utils/openapi-generator-cli.sh > ~/bin/openapitools/openapi-generator-cli
   chmod u+x ~/bin/openapitools/openapi-generator-cli
   export PATH=$PATH:~/bin/openapitools/
