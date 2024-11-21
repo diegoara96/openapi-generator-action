6b3245d3c620e212ee018d4f270956e1ccfeea9b
Diego Araujo <diegoaraujonovoa@gmail.com>
2024-11-21 19:37:36 +0100

    url o fichero

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 
+# Si se proporciona una URL de backend, descargar la especificación JSON
+if [ -n "$INPUT_URL" ]; then
+  echo "Descargando la especificación OpenAPI desde la URL proporcionada..."
+  
+  # Instalar dependencias solo si es necesario (solo si se usa la URL)
+  apt update
+  apt install -y curl jq
+  
+  # Descargar la especificación OpenAPI usando curl
+  curl -s $INPUT_URL -o spec.json
+fi
 
-mkdir -p ~/bin/openapitools
+# Si se proporciona un archivo JSON, usarlo directamente
+if [ -n "$INPUT_SPEC_FILE" ]; then
+  echo "Usando el archivo JSON proporcionado: $INPUT_SPEC_FILE"
+  cp $INPUT_SPEC_FILE spec.json
+fi
 
+# Si no se tiene un archivo `spec.json`, fallar
+if [ ! -f spec.json ]; then
+  echo "Error: No se proporcionó ni una URL ni un archivo JSON. Por favor, especifica uno de los dos."
+  exit 1
+fi
 
-apt update
-apt install -y curl maven jq
-curl https://raw.githubusercontent.com/OpenAPITools/openapi-generator/master/bin/utils/openapi-generator-cli.sh > ~/bin/openapitools/openapi-generator-cli
-
-
-chmod u+x ~/bin/openapitools/openapi-generator-cli
-
-export PATH=$PATH:~/bin/openapitools/
-
-curl $INPUT_URL > spec.json
-
+# Generar la documentación utilizando el generador proporcionado
+echo "Generando la documentación con OpenAPI Generator..."
 openapi-generator-cli generate -i spec.json -g $INPUT_GENERATOR -o $INPUT_OUTPUT_DIR
